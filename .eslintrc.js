fc77aca4891950d6cfe256f711afd106c9f84f68
Samuel Loup <loup@epsitec.ch>
2018-03-09 11:04:25 +0100

    Add repo config files

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    mocha: true,
+    node: true,
+    es6: true,
+  },
+  parser: 'babel-eslint',
+  plugins: ['react', 'babel'],
+  extends: ['prettier', 'eslint:recommended', 'plugin:react/recommended'],
+  rules: {
+    // Other rules
+    'no-console': 'off',
+    'valid-jsdoc': ['error', {requireReturn: false}],
+    eqeqeq: 'error',
+  },
+};
