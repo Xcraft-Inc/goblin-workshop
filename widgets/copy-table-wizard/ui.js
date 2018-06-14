080a4c4d7276d139c94a5646056d404808165f27
Samuel Loup <samuel.loup@gmail.com>
2018-06-14 10:50:55 +0200

    Better copy entity from mandate UI

--- widgets/copy-table-wizard/ui.js
+++ widgets/copy-table-wizard/ui.js
@@ -8,22 +8,35 @@ import Field from 'gadgets/field/widget';
 function prepare(props) {
   return (
     <Container kind="column" grow="1">
-      <Label text="Sélectionnez une base" />
+      <Label text="Sélectionnez le mandat source" />
       <Separator kind="space" height="10px" />
       <Field
         kind="combo"
-        labelText="Base de données"
+        labelText="Mandat"
         listModel=".form.databases"
         model=".form.fromDb"
       />
+      <Separator kind="space" height="10px" />
+      <Label text="Sélectionnez les entités à copier" />
       <Container kind="row">
         <Field
           kind="gadget"
           name="tablesTable"
           selectionMode="multi"
           frame="true"
+          hasButtons="true"
           height="300px"
           grow="1"
+        />
+      </Container>
+      <Separator kind="space" height="10px" />
+      <Label text="Options:" />
+      <Separator kind="space" height="10px" />
+      <Container kind="row">
+        <Field
+          kind="bool"
+          model=".form.reindex"
+          labelText="Réindexer les entités"
         />
       </Container>
     </Container>
