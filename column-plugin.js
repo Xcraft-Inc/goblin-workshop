742cb7ebb0f60da366261104be5ed3cc101524ed
Samuel Loup <samuel.loup@gmail.com>
2020-01-21 15:30:35 +0100

    add missing service file

---
+++ column-plugin.js
@@ -0,0 +1,15 @@
+'use strict';
+//T:2019-04-09
+
+const T = require('goblin-nabu/widgets/helpers/t.js');
+const {buildWorkitem} = require('goblin-workshop');
+
+const config = {
+  type: 'column',
+  kind: 'plugin',
+  title: '',
+};
+
+exports.xcraftCommands = function() {
+  return buildWorkitem(config);
+};
