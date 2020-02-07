a9078d5b15a746ddd60433e2614b0c842f268351
Samuel Loup <samuel.loup@gmail.com>
2020-02-07 16:44:02 +0100

    Refactor type discovery using entity schema

--- entities/column.js
+++ entities/column.js
@@ -30,71 +30,9 @@ const entity = {
       if (!path) {
         return;
       }
-      try {
-        const pathParts = path.split('.');
-        if (pathParts.length > 1) {
-          switch (pathParts[0]) {
-            case 'meta':
-              switch (pathParts[1]) {
-                case 'status':
-                  yield quest.me.change({
-                    path: 'type',
-                    newValue: 'enum',
-                    muteChanged: true,
-                  });
-                  break;
-                default:
-                  yield quest.me.change({
-                    path: 'type',
-                    newValue: 'string',
-                    muteChanged: true,
-                  });
-              }
-
-              break;
-            case 'sums':
-              switch (pathParts[1]) {
-                case 'base':
-                case 'cost':
-                case 'reward':
-                  yield quest.me.change({
-                    path: 'type',
-                    newValue: 'price',
-                    muteChanged: true,
-                  });
-                  break;
-                default:
-                  yield quest.me.change({
-                    path: 'type',
-                    newValue: 'number',
-                    muteChanged: true,
-                  });
-              }
-              break;
-          }
-        } else {
-          const conf = buildEntity.configurations[entityType];
-          if (conf && conf.properties) {
-            yield quest.me.change({
-              path: 'type',
-              newValue: conf.properties[path].type || 'string',
-              muteChanged: true,
-            });
-          } else {
-            yield quest.me.change({
-              path: 'type',
-              newValue: 'string',
-              muteChanged: true,
-            });
-          }
-        }
-      } catch {
-        yield quest.me.change({
-          path: 'type',
-          newValue: 'invalid',
-          muteChanged: true,
-        });
-      }
+      const schemaAPI = quest.getAPI(`entity-schema@${entityType}`);
+      const type = yield schemaAPI.getType({path});
+      yield quest.me.change({path: 'type', newValue: type});
     },
   },
 
