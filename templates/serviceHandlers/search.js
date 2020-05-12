a0ecae5b16885619e719b7c0049640cea0ffdac2
Samuel Loup <samuel.loup@gmail.com>
2020-05-12 14:17:57 +0200

    add helpers for hinter onValidate

--- templates/serviceHandlers/search.js
+++ templates/serviceHandlers/search.js
@@ -1,7 +1,7 @@
 module.exports = (type) => `'use strict';
 
 const T = require('goblin-nabu/widgets/helpers/t.js');
-const {buildWorkitem} = require('goblin-workshop');
+const {buildWorkitem, editSelectedEntityQuest} = require('goblin-workshop');
 
 const config = {
   type: '${type}',
@@ -11,26 +11,7 @@ const config = {
   list: "${type}",
   hinters: {
     ${type}: {
-      onValidate: function*(quest, selection, currentLocation) {
-        const desk = quest.getAPI(quest.goblin.getX('desktopId'));
-        const ${type} = yield quest.me.getEntity({
-          entityId: selection.value,
-          privateState: true,
-        });
-        yield desk.addWorkitem({
-          workitem: {
-            id: ${type}.get('id'),
-            name: '${type}-workitem',
-            description: ${type}.get('meta.summaries.info'),
-            view: 'default',
-            kind: 'tab',
-            icon: 'solid/pencil',
-            isClosable: true,
-          },
-          navigate: true,
-          currentLocation,
-        });
-      },
+      onValidate: editSelectedEntityQuest('${type}-workitem'),
     },
   },
 };
