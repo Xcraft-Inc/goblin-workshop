a04b411f739e016a6beeca9d4ea6e7fd5b38da6a
Samuel Loup <samuel.loup@gmail.com>
2020-10-29 09:44:01 +0100

    entity-view: load schema in UI when the workitem load

--- widgets/view-workitem/service.js
+++ widgets/view-workitem/service.js
@@ -6,6 +6,12 @@ const {buildWorkitem} = require('goblin-workshop');
 const config = {
   type: 'view',
   kind: 'workitem',
+  onLoad: function* (quest) {
+    //Load entity schema related to this view
+    const viewId = quest.goblin.getX('entityId');
+    const schemaId = `entity-schema@${viewId.split('@')[1]}`;
+    yield quest.create(schemaId, {id: schemaId, desktopId: quest.getDesktop()});
+  },
   onUpdate: function* (quest) {
     const entityAPI = quest.getAPI(quest.goblin.getX('entityId'));
     yield entityAPI.buildQuery();
