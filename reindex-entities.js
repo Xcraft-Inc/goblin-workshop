84a7cdf5331c829c97168c4b7f53509b75595395
Mathieu Schroeter <mathieu@schroetersa.ch>
2022-09-02 22:19:58 +0200

    Fix typo

--- reindex-entities.js
+++ reindex-entities.js
@@ -1,6 +1,6 @@
 const Goblin = require('xcraft-core-goblin');
 //usage:
-//  quest.evt(rehydrate-entities-enqueue-requested,{desktopId, data});
+//  quest.evt(reindex-entities-enqueue-requested,{desktopId, data});
 exports.xcraftCommands = function () {
   return Goblin.buildQueue('reindex-entities', {
     sub: '*::*.<reindex-entities-enqueue-requested>',
